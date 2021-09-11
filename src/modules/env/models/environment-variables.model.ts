df185945480adc7e6bf5af3bbbb5fc150cdfb00b
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-11 18:28:38 -0300

    feat(environment-variables.model.ts): created the "influx" related variables to allow connecting with it

--- src/modules/env/models/environment-variables.model.ts
+++ src/modules/env/models/environment-variables.model.ts
@@ -11,12 +11,93 @@ import {
  * the application starts.
  */
 export class EnvironmentVariables {
-  @IsDefined({ message: 'It is required to set the "NODE_ENV"' })
-  @IsString({ message: 'It is required to send a valid string value' })
+  @IsDefined({
+    message: 'It is required to set the "NODE_ENV"',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
   @IsIn(['test', 'development', 'production'])
   NODE_ENV: string
 
   @IsOptional()
-  @IsNumber({}, { message: 'It is required to send a number value' })
+  @IsNumber(
+    {},
+    {
+      message: 'It is required to set a number value',
+    },
+  )
   PORT?: number
+
+  //#region Influx
+
+  @IsDefined({
+    message: 'It is required to set the influx db',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_DB: string
+
+  @IsDefined({
+    message: 'It is required to set the influx user',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_USER: string
+
+  @IsDefined({
+    message: 'It is required to set the influx password',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_USER_PASSWORD: string
+
+  @IsDefined({
+    message: 'It is required to set the influx bucket',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_BUCKET: string
+
+  @IsDefined({
+    message: 'It is required to set the influx org',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_ORG: string
+
+  @IsDefined({
+    message: 'It is required to set the influx password',
+  })
+  @IsNumber(
+    {
+      maxDecimalPlaces: 0,
+    },
+    {
+      message: 'It is required to set a valid number value',
+    },
+  )
+  INFLUXDB_PORT: number
+
+  @IsDefined({
+    message: 'It is required to set the influx host',
+  })
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  INFLUXDB_HOST: string
+
+  @IsOptional()
+  @IsString({
+    message: 'It is required to set a valid string value',
+  })
+  @IsIn(['http', 'https'])
+  INFLUXDB_PROTOCOL?: 'http' | 'https'
+
+  //#endregion
 }
