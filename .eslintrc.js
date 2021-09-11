da7570672ec6eeaaf3f832f686457901211f8002
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-11 13:26:17 -0300

    build: setup the project linting and organization

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,47 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['tsconfig.json'],
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import-helpers'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/@nestjs|^typeorm|@sentry|^@graphql-example/',
+          '/.types/',
+          '/.decorator/',
+          '/.guard/',
+          '/.pipe/',
+          '/.exception/',
+          '/.entity/',
+          '/.dto|.enum|.model|.input|.args/',
+          '/.service/',
+          '/.controller/',
+          '/utils/',
+          ['index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
   },
-};
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+
+import { AppService } from './app.service'
+
+import { AppController } from './app.controller'
 
 @Module({
   imports: [],
