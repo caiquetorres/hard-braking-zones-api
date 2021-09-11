cac0a0ce5b32209d10a5c5e3f775ed928f021e74
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-11 16:50:21 -0300

    feat(app.module.ts): setup the "InfluxModule" and the "EnvModule"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common'
 
+import { InfluxConfigService } from './config/influx/influx-config.service'
+
+import { EnvModule } from './modules/env/env.module'
+import { InfluxModule } from './modules/influx/influx.module'
+
 @Module({
-  imports: [],
+  imports: [
+    EnvModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    InfluxModule.forRootAsync({
+      useClass: InfluxConfigService,
+    }),
+  ],
 })
 export class AppModule {}
