0d518b0173c3d8a39a2f091b6ac44b206524d263
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-25 15:45:53 -0300

    feat(auth.controller.ts & auth.service.ts): created the "login" and the "refresh" methods

--- src/modules/auth/dtos/login.dto.ts
+++ src/modules/auth/dtos/login.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjsx/crud/lib/crud'
+
+import { IsDefined, IsEmail, IsString } from 'class-validator'
+
+/**
+ * Dto that represents the data sent to the backend to perform the login.
+ */
 export class LoginDto {
+  @ApiProperty()
+  @IsDefined({ message: 'It is required to send the email' })
+  @IsString({ message: 'It is required to send a valid string' })
+  @IsEmail({}, { message: 'It is requried to send a valid email' })
   email: string
+
+  @ApiProperty()
+  @IsDefined({ message: 'It is required to send the password' })
+  @IsString({ message: 'It is required to send a valid string' })
   password: string
 }
