5f2dccef77a078f7688ea2c7d62979195779d17e
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-29 17:57:30 -0300

    feat(entity-already-disabled.exception.ts & entity-already-enabled.exception.ts): created these classes

---
+++ src/exceptions/conflict/entity-already-enabled.exception.ts
@@ -0,0 +1,25 @@
+import { HttpException, HttpStatus, Type } from '@nestjs/common'
+
+import { BaseEntity } from '../../shared/base.entity'
+
+/**
+ * Instantiate a EntityAlreadyEnabledException Exception.
+ *
+ * @example
+ * ```typescript
+ * throw new EntityAlreadyEnabledException(1, EntityType)
+ * ```
+ *
+ * @param identifier defines the entity id or unique identifier value
+ * @param type defines the entity type
+ */
+export class EntityAlreadyEnabledException extends HttpException {
+  public constructor(identifier: number | string, type?: Type<BaseEntity>) {
+    super(
+      `The entity identified by "${identifier}"${
+        type ? ` of type "${type.name}"` : ''
+      } is already enabled`,
+      HttpStatus.CONFLICT,
+    )
+  }
+}
