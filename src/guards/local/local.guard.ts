0bf62f076aeafd04963e94dec0f4ecd0b33a169c
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-29 07:13:44 -0300

    docs(jwt.guard.ts & local.guard.ts): added some docs to these guards

--- src/guards/local/local.guard.ts
+++ src/guards/local/local.guard.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
+/**
+ * Guard responsible for protecting routes using the `local` strategy.
+ */
 @Injectable()
 export class LocalGuard extends AuthGuard('local') {}
