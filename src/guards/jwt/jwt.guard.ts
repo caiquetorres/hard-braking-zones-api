b8e7ae1cbe66ca697649cb3fe3c4dbafeebae297
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-30 19:38:31 -0300

    fix(jwt.guard.ts & local.guard.ts): changed the error message when these guards rejects something

--- src/guards/jwt/jwt.guard.ts
+++ src/guards/jwt/jwt.guard.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 /**
  * Guard responsible for protecting routes using the jwt strategy.
  */
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+  /**
+   * Method that handles the request and deals with the user and error data.
+   *
+   * @param error defines an object that represents the error.
+   * @param user defines an object that represents the logged user.
+   * @returns an object that represents the logged user.
+   */
+  public handleRequest<T>(error: Error, user: T): T {
+    if (error || !user) {
+      throw new UnauthorizedException('The username or password are wrong')
+    }
+
+    return user
+  }
+}
