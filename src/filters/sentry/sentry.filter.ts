6df5a3cad2a7bf7271327ad7b39dff41fe18397d
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-30 20:20:17 -0300

    feat(sentry.filter.ts & environment-variables.model.ts): created the "SentryFilter" class; setup all the needed sentry information

---
+++ src/filters/sentry/sentry.filter.ts
@@ -0,0 +1,54 @@
+import {
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
+import * as Sentry from '@sentry/node'
+
+import { EnvService } from '../../modules/env/services/env.service'
+
+/**
+ * Class that represents the filter that capture some exception and send
+ * it to sentry.io
+ */
+@Catch()
+export class SentryFilter implements ExceptionFilter {
+  /**
+   * Property that defines if the sentry is enabled or not for this
+   * application.
+   */
+  private readonly sentryEnabled: boolean
+
+  constructor(envService: EnvService) {
+    this.sentryEnabled =
+      envService.get('SENTRY_ENABLED') && !!envService.get('SENTRY_DSN')
+
+    if (this.sentryEnabled) {
+      Sentry.init({
+        environment: envService.get('NODE_ENV'),
+        dsn: envService.get('SENTRY_DSN'),
+        release: envService.get('PACKAGE_VERSION'),
+      })
+    }
+  }
+
+  /**
+   * Method that deals with the thrown exceptions
+   *
+   * @param exception defines and object that represents the thrown exception
+   * @param host defines and object that represents the host arguments
+   */
+  catch(exception: HttpException): HttpException {
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+
+    if (status >= 500 && this.sentryEnabled) {
+      Sentry.captureException(exception)
+    }
+
+    return exception
+  }
+}
