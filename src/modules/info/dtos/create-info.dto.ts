7513eb4559143b9c881e36b73c08a40d836b3476
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-10-04 07:27:21 -0300

    fix(create-info.dto.ts & update-info.dto.ts): fixed the "text" property validation

--- src/modules/info/dtos/create-info.dto.ts
+++ src/modules/info/dtos/create-info.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger'
 
+import { IsDefined, IsString } from 'class-validator'
+
 export class CreateInfoDto {
   @ApiProperty()
+  @IsDefined({ message: 'It is required to send the text' })
+  @IsString({ message: 'It is required to send a valid string' })
   text: string
 }
--- src/modules/info/dtos/update-info.dto.ts
+++ src/modules/info/dtos/update-info.dto.ts
@@ -1,6 +1,10 @@
 import { ApiPropertyOptional } from '@nestjs/swagger'
 
+import { IsOptional, IsString } from 'class-validator'
+
 export class UpdateInfoDto {
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: 'It is required to send a valid string' })
   text?: string
 }
