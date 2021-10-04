6408b68331c5707e505e87828e163b31595995d6
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-10-04 07:05:50 -0300

    feat(about.module.ts): setup the "AboutEntity" in "AboutModule" imports

--- src/modules/about/about.module.ts
+++ src/modules/about/about.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AboutEntity } from './entities/about.entity'
 
 import { AboutService } from './about.service'
 
 import { AboutController } from './about.controller'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AboutEntity])],
   controllers: [AboutController],
   providers: [AboutService],
 })
